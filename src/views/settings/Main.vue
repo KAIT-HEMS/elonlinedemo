72c7c6382e469ce26b9a2fdb694cc426878f206a
Rakesh Kumar <justdecodeme@hotmail.com>
2024-08-02 17:06:19 +0530

    Update the logic for Point A > Point C

--- src/views/settings/Main.vue
+++ src/views/settings/Main.vue
@@ -295,18 +295,10 @@
               <div class="card shadow p-3 d-grid gap-3">
                 <h3 class="fs-6 text-primary fw-normal">0x0287  {{ getClassName(0x0287) }}</h3>
                 <section class="grid grid-template-max-3">
-                  <div v-show="isRHEForEVChargerDischargerSystem">
+                  <div>
                     <div class="input-group">
                       <div class="input-group-text">C</div>
                       <select class="form-select" v-model.number="evChargerDischargerSystemPointCRef">
-                        <option v-for="(epc, index) of [208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240]" :key="index" :value="epc">CH{{ index + 1 }} ({{ epc.toHex(2).toUpperCase().prefix('0x') }})</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div v-show="!isRHEForEVChargerDischargerSystem">
-                    <div class="input-group">
-                      <div class="input-group-text">A</div>
-                      <select class="form-select" v-model.number="evChargerDischargerSystemPointARef">
                         <option v-for="(epc, index) of [208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240]" :key="index" :value="epc">CH{{ index + 1 }} ({{ epc.toHex(2).toUpperCase().prefix('0x') }})</option>
                       </select>
                     </div>
@@ -568,7 +560,6 @@ export default defineComponent({
           isRealDevicesForEVChargerDischargerSystem          = computed(() => store.state.evChargerDischargerSystemMode === 'real' ? true : false),
           isRHEForEVChargerDischargerSystem                  = computed(() => store.state.evChargerDischargerSystemMode === 'rhe' ? true : false),
           evChargerDischargerSystemModeRef   = ref<string>(store.state.evChargerDischargerSystemMode),
-          evChargerDischargerSystemPointARef = ref<number>(store.state.evChargerDischargerSystemPointA),
           evChargerDischargerSystemPointCRef = ref<number>(store.state.evChargerDischargerSystemPointC),
           evChargerDischargerSystemPointDRef = ref<number>(store.state.evChargerDischargerSystemPointD),
           evChargerDischargerSystemPointERef = ref<number>(store.state.evChargerDischargerSystemPointE),
@@ -748,7 +739,7 @@ export default defineComponent({
       store.commit('setBatterySystemMode', batterySystemModeRef.value);
       switch (batterySystemModeRef.value) {
         case 'real':
-          batterySystemPointCRef.value = 0xE7;
+          batterySystemPointCRef.value = 0xD2;
           batterySystemPointDRef.value = 0xD8;
           batterySystemPointERef.value = 0xD9;
           break;
@@ -782,22 +773,16 @@ export default defineComponent({
       store.commit('setEVChargerDischargerSystemMode', evChargerDischargerSystemModeRef.value);
       switch (evChargerDischargerSystemModeRef.value) {
         case 'real':
-          evChargerDischargerSystemPointARef.value = 0xD2;
-          evChargerDischargerSystemPointCRef.value = 0xE7;
+          evChargerDischargerSystemPointCRef.value = 0xD2;
           evChargerDischargerSystemPointDRef.value = 0xD8;
           evChargerDischargerSystemPointERef.value = 0xD9;
           break;
         case 'rhe':
-          evChargerDischargerSystemPointARef.value = 0xD2;
           evChargerDischargerSystemPointCRef.value = 0xEF;
           evChargerDischargerSystemPointDRef.value = 0xD6;
           evChargerDischargerSystemPointERef.value = 0xD7;
           break;
       }
-    });
-
-    watch(evChargerDischargerSystemPointARef, () => {
-      store.commit('setEVChargerDischargerSystemPointA', evChargerDischargerSystemPointARef.value);
     });
 
     watch(evChargerDischargerSystemPointCRef, () => {
@@ -917,7 +902,6 @@ export default defineComponent({
       isRealDevicesForEVChargerDischargerSystem,
       isRHEForEVChargerDischargerSystem,
       evChargerDischargerSystemModeRef,
-      evChargerDischargerSystemPointARef,
       evChargerDischargerSystemPointCRef,
       evChargerDischargerSystemPointDRef,
       evChargerDischargerSystemPointERef,
